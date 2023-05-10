17795a609ddea2a3e6b922a45327b47875219bd0
konarbalaji <konar.balaji@gmail.com>
2023-05-10 15:09:54 +0530

    Added code & test classes for implementing parallelism with CompletableFuture

--- MultiThreadingJavaProject/src/main/java/com/learnjava/completableFuture/ProductServiceUsingCompletableFuture.java
+++ MultiThreadingJavaProject/src/main/java/com/learnjava/completableFuture/ProductServiceUsingCompletableFuture.java
@@ -1,12 +1,13 @@
 package com.learnjava.completableFuture;
 
-import com.learnjava.domain.Product;
-import com.learnjava.domain.ProductInfo;
-import com.learnjava.domain.Review;
+import com.learnjava.domain.*;
+import com.learnjava.service.InventoryService;
 import com.learnjava.service.ProductInfoService;
 import com.learnjava.service.ReviewService;
 
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 import static com.learnjava.util.CommonUtil.stopWatch;
 import static com.learnjava.util.LoggerUtil.log;
@@ -15,10 +16,17 @@ public class ProductServiceUsingCompletableFuture {
 
     private ProductInfoService productInfoService;
     private ReviewService reviewService;
+    private InventoryService inventoryService;
 
     public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService) {
         this.productInfoService = productInfoService;
         this.reviewService = reviewService;
+    }
+
+    public ProductServiceUsingCompletableFuture(ProductInfoService productInfoService, ReviewService reviewService, InventoryService inventoryService) {
+        this.productInfoService = productInfoService;
+        this.reviewService = reviewService;
+        this.inventoryService = inventoryService;
     }
 
     public Product retrieveProductDetails(String productId){
@@ -52,7 +60,8 @@ public class ProductServiceUsingCompletableFuture {
     public static void main(String[] args) {
         ProductInfoService productInfoService = new ProductInfoService();
         ReviewService reviewService = new ReviewService();
-        ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService);
+        InventoryService inventoryService = new InventoryService();
+        ProductServiceUsingCompletableFuture productService = new ProductServiceUsingCompletableFuture(productInfoService, reviewService, inventoryService);
         String productId = "ABC123";
         Product product = productService.retrieveProductDetails(productId);
         log("Product is : " + product);
@@ -62,8 +71,16 @@ public class ProductServiceUsingCompletableFuture {
     public Product retrieveProductDetailsWithInventory(String productId){
         stopWatch.start();
 
-        CompletableFuture<ProductInfo> prodFut = CompletableFuture.supplyAsync(() -> productInfoService.retrieveProductInfo(productId));
-        CompletableFuture<Review> rev = CompletableFuture.supplyAsync(() -> reviewService.retrieveReviews(productId));
+        CompletableFuture<ProductInfo> prodFut = CompletableFuture
+                .supplyAsync(() -> productInfoService.retrieveProductInfo(productId))
+                .thenApply(prodInfo -> {
+                    prodInfo.setProductOptions(updateInventory(prodInfo));
+                    return prodInfo;
+                });
+
+
+        CompletableFuture<Review> rev = CompletableFuture
+                .supplyAsync(() -> reviewService.retrieveReviews(productId));
 
         Product prod = prodFut.thenCombine(rev, (productInfo, review) -> new Product(productId, productInfo, review)).join();
 
@@ -71,5 +88,26 @@ public class ProductServiceUsingCompletableFuture {
 
         log("Total TimeTaken : " + stopWatch.getTime());
         return prod;
+    }
+
+    private List<ProductOption> updateInventory(ProductInfo prodInfo) {
+
+        List<ProductOption> prodInfolist = prodInfo.getProductOptions()
+                                                .stream()
+                                                .map(productOption -> {
+                                                    Inventory inventory = inventoryService.retrieveInventory(productOption);
+                                                    productOption.setInventory(inventory);
+                                                    return productOption;
+                                                }).collect(Collectors.toList());
+
+        return prodInfolist;
+
+        /*prodInfo.getProductOptions()
+                    .forEach(prodOpt -> {
+                        prodOpt.setInventory(inventoryService.retrieveInventory(prodOpt));
+                    });
+
+        return prodInfo;*/
+
     }
 }
