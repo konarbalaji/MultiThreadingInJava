6974c719070663d41fa827e81b50e4bc5e3ed032
konarbalaji <konar.balaji@gmail.com>
2023-05-10 16:19:21 +0530

    Tested performance with multiple productOptions

--- MultiThreadingJavaProject/src/main/java/com/learnjava/service/ReviewService.java
+++ MultiThreadingJavaProject/src/main/java/com/learnjava/service/ReviewService.java
@@ -7,7 +7,7 @@ import static com.learnjava.util.CommonUtil.delay;
 public class ReviewService {
 
     public Review retrieveReviews(String productId){
-        delay(100);
+        delay(1000);
         return new Review(200, 4.5);
     }
 }
